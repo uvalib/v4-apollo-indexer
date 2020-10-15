ac6af265a28b0000a1e012b4feafdb137ca31b56
Michael Durbin <md5wz@virginia.edu>
2020-10-15 14:46:33 -0400

    VIRGONEW-1715: updated WSLS disclaimer

--- wsls-json-to-solr.rb
+++ wsls-json-to-solr.rb
@@ -245,7 +245,7 @@ def createXmlDoc(node, parent)
   pf '  <field name="shadowed_location_f_stored">VISIBLE</field>'
   pf '  <field name="library_f_stored">Special Collections</field>'
   pf '  <field name="terms_of_use_a">Each user of the WSLS materials must individually evaluate any copyright or privacy issues that might pertain to the intended uses of these materials, including fair use. &lt;a href="https://copyright.library.virginia.edu/wsls_use/"&gt;Read More.&lt;/a&gt;</field>'
-  pf '  <field name="notes_f_stored">Please note, some contents from this collection may contain harmful, offensive, or insensitive language. In an effort to represent resources as accurately as possible, library staff transcribe information exactly as it appears in its original context.</field>'
+  pf '  <field name="note_f_stored">Please note, some contents from this collection may contain harmful, offensive, or pejorative language. In an effort to reflect the collection as it has been presented to us, library staff transcribe information exactly as it appears in its original context.</field>'
   node["children"].each do |child|
     printSolrField child, node
   end
