96941ab5eb9a98062610ac3039ae8a0692fc0cb6
Michael Durbin <md5wz@virginia.edu>
2020-11-17 13:51:20 -0500

    VIRGONEW-1902: added generation of digital content service metadata.

--- dp-json-to-solr.rb
+++ dp-json-to-solr.rb
@@ -63,9 +63,9 @@ def printSolrField(node, parent)
   when "title"
     begin
       date=DateTime.parse(node["value"].gsub(/Daily Progress, /,'')).strftime('%Y-%m-%d')
-      pf "  <field name=\"title_tsearch_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"full_title_tsearchf_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"individual_call_number_a\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"published_daterange\">#{date}</field>\n  <field name=\"published_display_a\">#{date}</field>\n  <field name=\"published_date\">#{date}T00:00:00Z</field>"
+      pf "  <field name=\"title_tsearch_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"full_title_tsearchf_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"published_daterange\">#{date}</field>\n  <field name=\"published_display_a\">#{date}</field>\n  <field name=\"published_date\">#{date}T00:00:00Z</field>"
     rescue
-      pf "  <field name=\"title_tsearch_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"full_title_tsearchf_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"individual_call_number_a\">#{node['value'].encode(:xml => :text)}</field>"
+      pf "  <field name=\"title_tsearch_stored\">#{node['value'].encode(:xml => :text)}</field>\n  <field name=\"full_title_tsearchf_stored\">#{node['value'].encode(:xml => :text)}</field>"
     end
   when "externalPID"
     pf pidFields(node)
@@ -91,10 +91,26 @@ def getManifest(node)
 end
 
 def pidFields(node)
-    sequences=JSON.parse(getManifest(node)).values_at("sequences")
-    canvases=sequences[0][0]["canvases"]
-    thumbnail=canvases[0]["thumbnail"]
-    "  <field name=\"id\">#{node['value']}</field>\n <field name=\"alternate_id_f_stored\">#{node['value']}</field>\n <field name=\"url_oembed_stored\">https://curio.lib.virginia.edu/oembed?url=https://curio.lib.virginia.edu/view/#{node['value']}</field>\n <field name=\"rights_wrapper_url_a\">http://rightswrapper2.lib.virginia.edu:8090/rights-wrapper/?pid=#{node['value']}&amp;pagePid=</field>\n <field name=\"work_title3_key_ssort_stored\">unique_#{node['value']}</field>\n <field name=\"work_title2_key_ssort_stored\">unique_#{node['value']}</field>\n <field name=\"thumbnail_url_a\">#{thumbnail}</field>\n"
+  pid = node['value']
+  manifest = JSON.parse(getManifest(node))
+  sequences = manifest.values_at("sequences")
+  canvases=sequences[0][0]["canvases"]
+  thumbnail=canvases[0]["thumbnail"]
+  partLabel=manifest["label"].sub('Daily Progress Digitized Microfilm: Daily Progress, ', '')
+    
+  part = {}
+  part['iiif_manifest_url'] = "https://s3.us-east-1.amazonaws.com/iiif-manifest-cache-production/pid-#{pid.sub(':', '-')}"
+  part['oembed_url'] = "https://curio.lib.virginia.edu/oembed?url=https://curio.lib.virginia.edu/view/#{pid}"
+  part['label'] = partLabel
+  part['pdf'] = { 'urls' => { 'delete' => "https://pdfservice.lib.virginia.edu/pdf/#{pid}/delete", 'download' => "https://pdfservice.lib.virginia.edu/pdf/#{pid}/download", 'generate' => "https://pdfservice.lib.virginia.edu/pdf/#{pid}/generate", 'status' => "https://pdfservice.lib.virginia.edu/pdf/#{pid}/status"}}
+  part['pid'] = pid
+  part['thumbnail_url'] = thumbnail
+  metadata = {};
+  metadata['parts'] = []
+  metadata['parts'].push(part);
+  File.open("dp/dcs/#{pid}", "w").write(metadata.to_json)
+    "<field name=\"id\">#{pid}</field>\n<field name=\"work_title3_key_ssort_stored\">unique_#{pid}</field>\n<field name=\"work_title2_key_ssort_stored\">unique_#{pid}</field>\n<field name=\"digital_content_service_url_e_stored\">https://digital-content-metadata-cache-production.s3.amazonaws.com/#{pid}</field>\n<field name=\"thumbnail_url_a\">#{thumbnail}</field>"
+
 end
 
 def reelFields(node)
@@ -111,17 +127,15 @@ def createXmlDoc(node, parent)
   pf '  <field name="format_f">Online</field>'
   pf '  <field name="circulating_f">true</field>'
   pf '  <field name="call_number_tsearch_stored" />'
-  pf '  <field name="data_source_f_stored">daily progress</field>'
   pf '  <field name="digital_collection_f_stored">Daily Progress Digitized Microfilm</field>'
   pf '  <field name="shadowed_location_f_stored">VISIBLE</field>'
-  pf "  <field name=\"identifier_e_stored\"> #{$ancestorPIDs[0]}</field>"
-  pf "  <field name=\"identifier_e_stored\"> #{$ancestorPIDs[1]}</field>"
-  pf "  <field name=\"identifier_e_stored\"> #{$ancestorPIDs[2]}</field>"
+  pf "  <field name=\"identifier_e_stored\">#{$ancestorPIDs[0]}</field>"
+  pf "  <field name=\"identifier_e_stored\">#{$ancestorPIDs[1]}</field>"
+  pf "  <field name=\"identifier_e_stored\">#{$ancestorPIDs[2]}</field>"
   pf '  <field name="dailyprogress_tsearch">daily progress digitized microfilm digital scan newspaper charlottesville</field>'
-  pf '  <field name="pdf_url_a">https://pdfservice.lib.virginia.edu/pdf</field>'
+  pf '  <field name="url_str_stored">https://search-dev.lib.virginia.edu/search?mode=advanced&amp;q=keyword%3A%20%7B%2a%7D&amp;exclude=articles,books,images,jmrl,archival,maps,music-recordings,musical-scores,sound-recordings,thesis,video,worldcat&amp;pool=journals&amp;filter=%7B%22FacetCollection%22%3A%5B%22Daily%20Progress%20Digitized%20Microfilm%22%5D%7D&amp;sort=SortDatePublished_asc</field>'
+  pf '  <field name="url_label_str_stored">Browse all Issues</field>'
   pf '  <field name="feature_f_stored">iiif</field>'
-  pf '  <field name="feature_f_stored">rights_wrapper</field>'
-  pf '  <field name="feature_f_stored">pdf_service</field>'
   node["children"].each do |child|
     printSolrField child, node
   end
@@ -131,6 +145,7 @@ end
 
 #conf.echo = false
 FileUtils.mkdir_p 'dp/cache'
+FileUtils.mkdir_p 'dp/dcs'
 jsonFile = 'dp/uva-an1054'
 
 # get the file from apollo
